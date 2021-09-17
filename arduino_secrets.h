d4d6f382f5a516b920c880df30221ee312bf49d1
Moritz Gamperl <70139223+moritzgamperl@users.noreply.github.com>
2021-09-17 09:40:41 +0200

    deleted old file; removed APP KEY

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
 // Replace with keys obtained from TheThingsNetwork console
-#define SECRET_APP_EUI "01"
-#define SECRET_APP_KEY "01020304050607080910111213141501"
+#define SECRET_APP_EUI "xxxxxxxxxxxxxxxxxxxxxxx"
+#define SECRET_APP_KEY "yyyyyyyyyyyyyyyyyyyyyyy"
