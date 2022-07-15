2186d9e0d475b38af64a98bbd0af78c8cbeec575
Moritz Gamperl <70139223+moritzgamperl@users.noreply.github.com>
2022-07-15 17:10:53 +0200

    Update ADC_Config.h
    change to variable measurements for each channel

--- ADC_Config.h
+++ ADC_Config.h
@@ -6,11 +6,11 @@
 #define ADS1220_CS_PIN    5
 #define ADS1220_DRDY_PIN  4
 
-// SET ADS1220 channels; 0 = off; 1 = on // For the ADC CHIP
-bool ADS_C0 = true;
-bool ADS_C1 = false;
-bool ADS_C2 = false;
-bool ADS_C3 = false;
+// SET ADS1220 channels; 0 = off; 1 = single-ended measurement; 2 = single-ended measurement with 12V (4-20 mA)
+int ADS_C0 = 0;
+int ADS_C1 = 2;
+int ADS_C2 = 2;
+int ADS_C3 = 0;
 
 //ads1220.ads1220debug = 1;                      //TURN ON/OFF DEBUG MESSAGES
 
